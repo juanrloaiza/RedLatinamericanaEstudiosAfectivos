1014d8286e242d121196a90f8f00a93b6b3d2dc8
Juan R. Loaiza <juanrloaiza@gmail.com>
2025-11-17 15:31:34 -0300

    Implements languages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
     vite: { plugins: [tailwindcss()], },
+    i18n: {
+        locales: ["es", "en", "pt-br"],
+        defaultLocale: "es",
+        routing: {
+            prefixDefaultLocale: true
+        }
+    }
 });
